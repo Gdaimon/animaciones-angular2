da6045e086e893fb082e178f99bd88a29d59a3b2
Carlos Andrés Charris S <darkklitos@gmail.com>
2017-07-09 12:11:56 -0500

    Animaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from "@angular/animations";
+import { Component } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component ( {
+  selector  : "app-root",
+  animations: [
+    trigger ( "signal", [
+      state ( "void", style ( {
+        "transform": "translateY(-100%)"
+      } ) ),
+      state ( "go", style ( {
+        "background-color": "green",
+        "height"          : "100px"
+      } ) ),
+      state ( "stop", style ( {
+        "background-color": "red",
+        "height"          : "50px"
+      } ) ),
+      // transition ( "void => *", animate ( 500 ) ),
+      transition ( "* => *", animate ( "2s 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)" ) )
+    ] )
+  ],
+  styles    : [ `
+    .traffic-light {
+      width: 100px;
+      height: 100px;
+      background-color: black;
+    }
+  ` ],
+  template  : `
+    <div [@signal]="signal"
+         class="traffic-light"
+         *ngIf="isHere"
+    >
+    </div>
+    <button (click)="onGoClick()">Go</button>
+    <button (click)="onStopClick()">Stop</button>
+    <hr>
+    <button (click)="onToggleClick()">Toogle</button>
+  `
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+} )
 export class AppComponent {
-  title = 'app';
+  signal;
+  isHere = false;
+  
+  onGoClick () {
+    this.signal = "go";
+  }
+  
+  onStopClick () {
+    this.signal = "stop";
+  }
+  
+  onToggleClick () {
+    this.isHere = !this.isHere;
+    console.log ( this.isHere );
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-@NgModule({
+@NgModule ( {
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  imports     : [
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  providers   : [],
+  bootstrap   : [ AppComponent ]
+} )
+export class AppModule {
+}
